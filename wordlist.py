00e2d1947c0ccdc34c4bc310a9dfd1c5e306ec54
NightFox <87749296+MKinG4ever@users.noreply.github.com>
2023-02-16 21:55:01 +0330

    WordList Handler Script
    wordlist v0.3
    author: NightFox
    Open-Source

--- wordlist.py
+++ wordlist.py
@@ -5,14 +5,21 @@ from functools import wraps
 
 
 def exe_time(func):
-    """execution time, decorator"""
+    """
+        execution time, decorator
+        author: NightFox | exe_time Beta v0.1
+        e-mail: mking4everking@gmail.com
+        git-hub: @MKing4ever
+        GNU Open-Source Certification
+    """
 
     @wraps(func)
     def wrapper(*args, **kwargs):
         _start = time.time()
         result = func(*args, *kwargs)
         _end = time.time()
-        print(f'Execution Time: {_end - _start:.3}')
+        _total = _end - _start
+        print(f'ET: {_total:.3}')
         return result
 
     return wrapper
@@ -21,24 +28,29 @@ def exe_time(func):
 class WordList:
     """
         WordList Handler Script
-        author: NightFox | wordlist Beta v0.2.1
+        author: NightFox | wordlist Beta v0.3
         e-mail: mking4everking@gmail.com
         git-hub: @MKing4ever
         GNU Open-Source Certification
     """
 
-    # notification of features and changes
+    # type of 'words' and 'exceptions' changed to set()
+    # added: speed and version Variables.
+    # removed: duplicate Function.
 
     def __init__(self):
         # lists
-        self.words = ['pass', 'passwd', 'password']  # list | my wordlist
-        self.exceptions = [' ', '']  # list | my exceptions wordlist
+        self.words = {'password', 'passwd', 'pass'}  # set | my wordlist
+        self.exceptions = {' ', ''}  # set | my exceptions wordlist
         # file address
         self.address = './'
         self.filename = 'WordList.txt'
+        # about
+        self.speed = 0  # delay time for echo
+        self.version = 'v0.3'  # script version
 
     def __repr__(self):
-        return f'WordList Module v0.2.1\nLength: {self.__len__()}\nBackup: {self.exist}\nAddress: {self.file}'
+        return f'WordList Module {self.version}\nLength: {self.__len__()}\nBackup: {self.exist}\nAddress: {self.file}'
 
     def __str__(self):
         return self.list2str(self.data)
@@ -51,8 +63,8 @@ class WordList:
 
     @property
     def data(self):
-        self.words = self.duplicate(self.words)  # Difference between 'words' and 'data'
-        return self.words + self.exceptions
+        self.words.update(self.exceptions)  # exceptions can not add easily
+        return self.words
 
     @property
     def file(self):
@@ -60,31 +72,27 @@ class WordList:
 
     @property
     def exist(self):
-        return os.path.isfile(self.file)  # is "./WordList.txt" file & exist
+        return os.path.isfile(self.file)  # if "./WordList.txt" file & exist
 
     @staticmethod
-    def delay(secs=0):
+    def delay(secs=None):
         """make an interrupt"""
-        if secs:
-            time.sleep(secs)
+        if secs is None:
+            time.sleep(random.random() ** 5 / 3.141592653589793238)
         else:
-            time.sleep((random.random() / 3.1415926535).__pow__(2))
+            time.sleep(secs)
 
     @staticmethod
     def str2list(data: str) -> list:
         return data.split()  # split with ' '-(Space) and '\n'-(NewLine)
 
     @staticmethod
-    def list2str(data: list) -> str:
+    def list2str(data: list or set) -> str:
         return '\n'.join(data)  # make a True wordlist | merge words with '\n'-NewLine
 
     @staticmethod
-    def duplicate(data: list):
-        return sorted(list(set(data)))  # remove duplicates and return sorted list
-
-    @staticmethod
     def question(quest, answer):
-        """make sure answer is not ''(empty str) or just ' '(space)"""
+        """make sure answer is not ''(empty str) or just ' '(spaces)"""
         a = input(quest).strip()
         if a and a != answer:
             answer = a
@@ -98,7 +106,7 @@ class WordList:
                 chars += char
                 print(f'\r{chars}', end='')
                 if char != ' ':  # skip Spaces
-                    self.delay()
+                    self.delay(self.speed)
             print('')  # NewLine
 
     @exe_time
@@ -106,7 +114,7 @@ class WordList:
         if self.exist:
             with open(self.file, 'r') as file:
                 data = self.str2list(file.read())
-                self.words.extend(data)
+                self.words.update(data)
             self.echo(f'File \'{self.filename}\' imported. ({len(data)})')
         else:
             self.echo(f'No \'{self.filename}\' Found.')
@@ -149,13 +157,13 @@ class WordList:
     @exe_time
     def add_word(self, word: str):
         data = self.str2list(word)  # list | turn  str (input argument) to list
-        self.words.extend(data)  # update wordlist
+        self.words.update(data)  # update wordlist
         self.echo(f'Length: {self.__len__()}')
 
     @exe_time
     def add_words(self, words: list):
         w = [f'{i}' for i in words]
-        self.words.extend(w)  # update wordlist
+        self.words.update(w)  # update wordlist
         self.echo(f'Length: {self.__len__()}')
 
     def remove_word(self, word: str):
@@ -168,25 +176,27 @@ class WordList:
     def clear(self):
         self.echo('Clear list and back to default?')
         ans = input('Y/n/c | Default: Yes').lower().strip()
-        if ans in ['', ' ', 'y', 'yes']:
+        if ans in ['', 'y', 'yes']:
             self.address = './'  # back to default address
             self.filename = 'WordList.txt'  # default filename
-            self.words = self.exceptions = []  # Clear wordlist
+            self.words = {}  # Clear wordlist
+            self.exceptions = {' ', ''}
             self.echo('Done.')
         elif ans in ['n', 'no']:
             self.echo('Process suspended.')
         elif ans in ['c', 'clear']:
-            self.words = self.exceptions = []  # Clear wordlist
+            self.words = {}  # Clear wordlist
+            self.exceptions = {' ', ''}
             self.echo('wordlist cleared.')
         else:
             self.echo('Invalid input: Process canceled.')
 
     def info(self):
-        fn = ['data', 'file', 'exist', 'delay(secs)', 'str2list(data)', 'list2str(data)', 'duplicate(data)',
+        fn = ['data', 'file', 'exist', 'delay(secs)', 'str2list(data)', 'list2str(data)',
               'question(quest,answer)', 'echo(self, value)', 'import_list(self)',
               'export_list(self)', 'extend_list(self)', 'new_list(self)', 'delete_list(self)', 'read(self)',
               'add_word(self, word)', 'add_words(self, words)', 'remove_word(self,word)', 'clear(self)',
               'info(self)']
-        self.echo(f'- WordList Functions (Total:{len(fn)})')
+        self.echo(f'- WordList Functions (Total:{len(fn)})\n')
         print(self.list2str(['WordList.' + _name for _name in fn]))
         self.echo('for more information use \'help()\', \'dir()\' or __dir__ Built-in Functions')
